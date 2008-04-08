0bfca37a0367fa39545a99296915490474f5be2e
redsolo <redsolo@dev.java.net>
2008-04-08 21:59:52 +0000

    [ci-game-plugin] Implemented old newInstance() as other method NPE

--- src/main/java/hudson/plugins/cigame/UserScorePropertyDescriptor.java
+++ src/main/java/hudson/plugins/cigame/UserScorePropertyDescriptor.java
@@ -4,9 +4,12 @@ import net.sf.json.JSONObject;
 
 import org.kohsuke.stapler.StaplerRequest;
 
+import hudson.Util;
+import hudson.model.Hudson;
 import hudson.model.User;
 import hudson.model.UserProperty;
 import hudson.model.UserPropertyDescriptor;
+import hudson.model.Descriptor.FormException;
 
 /**
  * Descriptor for the {@link UserScoreProperty}.
@@ -27,13 +30,26 @@ public class UserScorePropertyDescriptor extends UserPropertyDescriptor {
 	}
 
 	@Override
+	public UserProperty newInstance(StaplerRequest req) throws FormException {
+		String scoreStr = Util.fixEmpty(req.getParameter("game.score"));
+		if (scoreStr != null) {
+			return new UserScoreProperty(Double.parseDouble(scoreStr));
+		} else {
+			return new UserScoreProperty();
+		}
+	}
+/*
+	@Override
 	public UserProperty newInstance(StaplerRequest req, JSONObject formData)
 			throws FormException {
+		if (formData != null) {
 		return new UserScoreProperty(formData.getDouble("game.score"));
+		} 
+		return null;
 	}
-
+*/
 	@Override
 	public UserProperty newInstance(User arg0) {
-		return new UserScoreProperty();
+		return null;
 	}
 }
