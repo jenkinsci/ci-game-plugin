b3dff39a08aea2980533c1f11f72d51e7345ce2e
redsolo <redsolo@dev.java.net>
2008-04-10 21:47:54 +0000

    [maven-release-plugin]  copy for tag ci-game-1.1

---
+++ src/test/java/hudson/plugins/cigame/rules/basic/BuildResultRuleTest.java
@@ -0,0 +1,72 @@
+package hudson.plugins.cigame.rules.basic;
+
+import hudson.model.Result;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class BuildResultRuleTest {
+
+	@Test
+	public void testFirstBuildSuccess() {
+		BuildResultRule rule = new BuildResultRule(100, -100);		
+		double points = rule.evaluate(Result.SUCCESS, null);
+		assertEquals("Successful build should give 100 points", 100, points);
+	}
+	
+	@Test
+	public void testFirstBuildFailed() {
+		BuildResultRule rule = new BuildResultRule(100, -100);		
+		double points = rule.evaluate(Result.FAILURE, null);
+		assertEquals("Failed build should give -100 points", -100, points);
+	}
+	
+	@Test
+	public void testFirstBuildWasUnstable() {
+		BuildResultRule rule = new BuildResultRule(100, -100);		
+		double points = rule.evaluate(Result.UNSTABLE, null);
+		assertEquals("Unstable build should give 0 points", 0, points);
+	}
+	
+	@Test
+	public void testLastBuildWasUnstable() {
+		BuildResultRule rule = new BuildResultRule(100, -100);		
+		double points = rule.evaluate(Result.SUCCESS, Result.UNSTABLE);
+		assertEquals("Fixed build should give 100 points", 100, points);
+	}
+	
+	@Test
+	public void testContinuedBuildFailure() {
+		BuildResultRule rule = new BuildResultRule(100, -100);		
+		double points = rule.evaluate(Result.FAILURE, Result.FAILURE);
+		assertEquals("No change in result should give 0 points", 0, points);
+	}
+	
+	@Test
+	public void testContinuedUnstableBuild() {
+		BuildResultRule rule = new BuildResultRule(100, -100);		
+		double points = rule.evaluate(Result.UNSTABLE, Result.UNSTABLE);
+		assertEquals("No change in result should give 0 points", 0, points);
+	}
+	
+	@Test
+	public void testLastBuildWasAborted() {
+		BuildResultRule rule = new BuildResultRule(100, -100);		
+		double points = rule.evaluate(Result.FAILURE, Result.ABORTED);
+		assertEquals("Previous aborted build should give 0 points", 0, points);
+	}
+	
+	@Test
+	public void testContinuedBuildSuccess() {
+		BuildResultRule rule = new BuildResultRule(100, -100);		
+		double points = rule.evaluate(Result.SUCCESS, Result.SUCCESS);
+		assertEquals("No change in result should give 100 points", 100, points);
+	}
+	
+	@Test
+	public void testCurrentBuildWasUnstable() {
+		BuildResultRule rule = new BuildResultRule(100, -100);		
+		double points = rule.evaluate(Result.UNSTABLE, Result.SUCCESS);
+		assertEquals("Unstable builds should give 0 points", 0, points);
+	}
+}
---
+++ src/test/java/hudson/plugins/cigame/rules/basic/IncreasingFailedTestsRuleTest.java
@@ -0,0 +1,43 @@
+package hudson.plugins.cigame.rules.basic;
+
+import static org.junit.Assert.*;
+import hudson.model.Result;
+
+import org.junit.Test;
+
+public class IncreasingFailedTestsRuleTest {
+	@Test
+	public void testNoTests() throws Exception {
+		IncreasingFailedTestsRule rule = new IncreasingFailedTestsRule(-10);
+		double points = rule.evaluate(Result.SUCCESS, Result.SUCCESS, 0, 0);
+		assertEquals("No new test should give 0 points", 0, points);
+	}
+	
+	@Test
+	public void testMoreFailingTests() throws Exception {
+		IncreasingFailedTestsRule rule = new IncreasingFailedTestsRule(-10);
+		double points = rule.evaluate(Result.UNSTABLE, Result.UNSTABLE, 2, 0);
+		assertEquals("2 new test should give -20 points", -20, points);
+	}
+	
+	@Test
+	public void testLessFailingTests() throws Exception {
+		IncreasingFailedTestsRule rule = new IncreasingFailedTestsRule(-10);
+		double points = rule.evaluate(Result.UNSTABLE, Result.UNSTABLE, 2, 4);
+		assertEquals("2 lost tests should give 0 points", 0, points);
+	}
+	
+	@Test
+	public void testPreviousBuildFailed() throws Exception {
+		IncreasingFailedTestsRule rule = new IncreasingFailedTestsRule(-10);
+		double points = rule.evaluate(Result.UNSTABLE, Result.FAILURE, 1, 0);
+		assertEquals("Previous failed build should give 0 points", 0, points);
+	}
+	
+	@Test
+	public void testCurrentBuildFailed() throws Exception {
+		IncreasingFailedTestsRule rule = new IncreasingFailedTestsRule(-10);
+		double points = rule.evaluate(Result.FAILURE, Result.UNSTABLE, 1, 0);
+		assertEquals("Current build failed should give 0 points", 0, points);
+	}
+}
