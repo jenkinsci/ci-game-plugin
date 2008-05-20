259017f0db33daa322b2b7508de92de863947181
redsolo <redsolo@dev.java.net>
2008-05-20 18:33:34 +0000

    [cigame-plugin] Fixed tests

--- src/test/java/hudson/plugins/cigame/rules/unittesting/IncreasingFailedTestsRuleTest.java
+++ src/test/java/hudson/plugins/cigame/rules/unittesting/IncreasingFailedTestsRuleTest.java
@@ -2,44 +2,44 @@ package hudson.plugins.cigame.rules.unittesting;
 
 import static org.junit.Assert.*;
 import hudson.model.Result;
+import hudson.plugins.cigame.model.RuleResult;
 import hudson.plugins.cigame.rules.unittesting.IncreasingFailedTestsRule;
 
 import org.junit.Test;
 
 public class IncreasingFailedTestsRuleTest {
-/*	@Test
-	public void testNoTests() throws Exception {
-		IncreasingFailedTestsRule rule = new IncreasingFailedTestsRule(-10);
-		double points = rule.evaluate(Result.SUCCESS, Result.SUCCESS, 0, 0);
-		assertEquals("No new test should give 0 points", 0, points);
-	}
-	
-	@Test
-	public void testMoreFailingTests() throws Exception {
-		IncreasingFailedTestsRule rule = new IncreasingFailedTestsRule(-10);
-		double points = rule.evaluate(Result.UNSTABLE, Result.UNSTABLE, 2, 0);
-		assertEquals("2 new test should give -20 points", -20, points);
-	}
-	
-	@Test
-	public void testLessFailingTests() throws Exception {
-		IncreasingFailedTestsRule rule = new IncreasingFailedTestsRule(-10);
-		double points = rule.evaluate(Result.UNSTABLE, Result.UNSTABLE, 2, 4);
-		assertEquals("2 lost tests should give 0 points", 0, points);
-	}
-	
-	@Test
-	public void testPreviousBuildFailed() throws Exception {
-		IncreasingFailedTestsRule rule = new IncreasingFailedTestsRule(-10);
-		double points = rule.evaluate(Result.UNSTABLE, Result.FAILURE, 1, 0);
-		assertEquals("Previous failed build should give 0 points", 0, points);
-	}
-	
-	@Test
-	public void testCurrentBuildFailed() throws Exception {
-		IncreasingFailedTestsRule rule = new IncreasingFailedTestsRule(-10);
-		double points = rule.evaluate(Result.FAILURE, Result.UNSTABLE, 1, 0);
-		assertEquals("Current build failed should give 0 points", 0, points);
-	}
-	*/
+    @Test
+    public void testNoTests() throws Exception {
+        IncreasingFailedTestsRule rule = new IncreasingFailedTestsRule(-10);
+        RuleResult result = rule.evaluate(Result.SUCCESS, Result.SUCCESS, 0, 0);
+        assertNull("No new test should return null", result);
+    }
+
+    @Test
+    public void testMoreFailingTests() throws Exception {
+        IncreasingFailedTestsRule rule = new IncreasingFailedTestsRule(-10);
+        RuleResult result = rule.evaluate(Result.UNSTABLE, Result.UNSTABLE, 2, 0);
+        assertEquals("2 new test should give -20 result", -20, result.getPoints());
+    }
+
+    @Test
+    public void testLessFailingTests() throws Exception {
+        IncreasingFailedTestsRule rule = new IncreasingFailedTestsRule(-10);
+        RuleResult result = rule.evaluate(Result.UNSTABLE, Result.UNSTABLE, 2, 4);
+        assertNull("2 lost tests should return null", result);
+    }
+
+    @Test
+    public void testPreviousBuildFailed() throws Exception {
+        IncreasingFailedTestsRule rule = new IncreasingFailedTestsRule(-10);
+        RuleResult result = rule.evaluate(Result.UNSTABLE, Result.FAILURE, 1, 0);
+        assertNull("Previous failed build should return null", result);
+    }
+
+    @Test
+    public void testCurrentBuildFailed() throws Exception {
+        IncreasingFailedTestsRule rule = new IncreasingFailedTestsRule(-10);
+        RuleResult result = rule.evaluate(Result.FAILURE, Result.UNSTABLE, 1, 0);
+        assertNull("Current build failed should return null", result);
+    }
 }
--- src/test/java/hudson/plugins/cigame/rules/unittesting/IncreasingPassedTestsRuleTest.java
+++ src/test/java/hudson/plugins/cigame/rules/unittesting/IncreasingPassedTestsRuleTest.java
@@ -1,51 +1,46 @@
 package hudson.plugins.cigame.rules.unittesting;
 
 import hudson.model.Result;
+import hudson.plugins.cigame.model.RuleResult;
 import hudson.plugins.cigame.rules.unittesting.IncreasingPassedTestsRule;
 
 import org.junit.Assert;
 import org.junit.Test;
 
 public class IncreasingPassedTestsRuleTest {
+
     @Test
-    public void test() {
-        Assert.assertEquals("2 = two", String.format("%d = two", 2));
-        Assert.assertEquals("2 = two", String.format("%.0f = two", (double)2));
+    public void testNoTests() throws Exception {
+        IncreasingPassedTestsRule rule = new IncreasingPassedTestsRule(10);
+        RuleResult result = rule.evaluate(Result.SUCCESS, Result.SUCCESS, 0, 0);
+        Assert.assertNull("No new test should return null", result);
     }
-/*
-	@Test
-	public void testNoTests() throws Exception {
-		IncreasingPassedTestsRule rule = new IncreasingPassedTestsRule(10);
-		double points = rule.evaluate(Result.SUCCESS, Result.SUCCESS, 0, 0);
-		Assert.assertEquals("No new test should give 0 points", 0, points);
-	}
-	
-	@Test
-	public void testMorePassingTests() throws Exception {
-		IncreasingPassedTestsRule rule = new IncreasingPassedTestsRule(10);
-		double points = rule.evaluate(Result.UNSTABLE, Result.UNSTABLE, 2, 0);
-		Assert.assertEquals("2 new test should give 20 points", 20, points);
-	}
-	
-	@Test
-	public void testLessPassingTests() throws Exception {
-		IncreasingPassedTestsRule rule = new IncreasingPassedTestsRule(10);
-		double points = rule.evaluate(Result.UNSTABLE, Result.UNSTABLE, 2, 4);
-		Assert.assertEquals("2 lost tests should give 0 points", 0, points);
-	}
-	
-	@Test
-	public void testPreviousBuildFailed() throws Exception {
-		IncreasingPassedTestsRule rule = new IncreasingPassedTestsRule(10);
-		double points = rule.evaluate(Result.UNSTABLE, Result.FAILURE, 1, 0);
-		Assert.assertEquals("Previous failed build should give 0 points", 0, points);
-	}
-	
-	@Test
-	public void testCurrentBuildFailed() throws Exception {
-		IncreasingPassedTestsRule rule = new IncreasingPassedTestsRule(10);
-		double points = rule.evaluate(Result.FAILURE, Result.UNSTABLE, 1, 0);
-		Assert.assertEquals("Current build failed should give 0 points", 0, points);
-	}
-	*/
+
+    @Test
+    public void testMorePassingTests() throws Exception {
+        IncreasingPassedTestsRule rule = new IncreasingPassedTestsRule(10);
+        RuleResult result = rule.evaluate(Result.UNSTABLE, Result.UNSTABLE, 2, 0);
+        Assert.assertEquals("2 new test should give 20 result", 20, result.getPoints());
+    }
+
+    @Test
+    public void testLessPassingTests() throws Exception {
+        IncreasingPassedTestsRule rule = new IncreasingPassedTestsRule(10);
+        RuleResult result = rule.evaluate(Result.UNSTABLE, Result.UNSTABLE, 2, 4);
+        Assert.assertNull("2 lost tests should return null", result);
+    }
+
+    @Test
+    public void testPreviousBuildFailed() throws Exception {
+        IncreasingPassedTestsRule rule = new IncreasingPassedTestsRule(10);
+        RuleResult result = rule.evaluate(Result.UNSTABLE, Result.FAILURE, 1, 0);
+        Assert.assertNull("Previous failed build should return null", result);
+    }
+
+    @Test
+    public void testCurrentBuildFailed() throws Exception {
+        IncreasingPassedTestsRule rule = new IncreasingPassedTestsRule(10);
+        RuleResult result = rule.evaluate(Result.FAILURE, Result.UNSTABLE, 1, 0);
+        Assert.assertNull("Current build failed should return null", result);
+    }
 }
