ecc51b454a09ba5d778fe2e57696a12087458ad5
redsolo <redsolo@dev.java.net>
2008-05-28 09:28:14 +0000

    [cigame-plugin] More fine grained rules for PMD plugin

--- src/main/java/hudson/plugins/cigame/rules/plugins/pmd/DefaultPmdRule.java
+++ src/main/java/hudson/plugins/cigame/rules/plugins/pmd/DefaultPmdRule.java
@@ -1,36 +1,57 @@
 package hudson.plugins.cigame.rules.plugins.pmd;
 
+import java.util.Collection;
 import java.util.List;
 
 import hudson.model.AbstractBuild;
+import hudson.model.Result;
 import hudson.plugins.cigame.model.Rule;
 import hudson.plugins.cigame.model.RuleResult;
 import hudson.plugins.pmd.PmdResult;
 import hudson.plugins.pmd.PmdResultAction;
+import hudson.plugins.pmd.util.model.Priority;
 
 public class DefaultPmdRule implements Rule {
+    private int pointsForAddingAnAnnotation;
+    private int pointsForRemovingAnAnnotation;
+
+    private Priority tasksPriority;
+
+    public DefaultPmdRule(Priority tasksPriority, int pointsForAddingAnAnnotation, int pointsForRemovingAnAnnotation) {
+        this.tasksPriority = tasksPriority;
+        this.pointsForAddingAnAnnotation = pointsForAddingAnAnnotation;
+        this.pointsForRemovingAnAnnotation = pointsForRemovingAnAnnotation;
+    }
 
     public RuleResult evaluate(AbstractBuild<?, ?> build) {
         int delta = 0;
-        List<PmdResultAction> actions = build.getActions(PmdResultAction.class);
-        for (PmdResultAction action : actions) {
-            PmdResultAction previousResultAction = action.getPreviousResultAction();
-            if (previousResultAction != null) {
-                PmdResult pmdResult = action.getResult();
-                delta += pmdResult.getDelta();
+        if ((build.getResult().isBetterOrEqualTo(Result.UNSTABLE)) 
+                && (build.getPreviousBuild() != null)) {
+            List<PmdResultAction> actions = build.getActions(PmdResultAction.class);
+            for (PmdResultAction action : actions) {
+                PmdResultAction previousResultAction = action.getPreviousResultAction();
+                if (previousResultAction != null) {
+                    PmdResult result = action.getResult();
+                    PmdResult previousResult = action.getPreviousResultAction().getResult();
+                    
+                    int annotations = result.getNumberOfAnnotations(tasksPriority);
+                    int previousAnnotations = previousResult.getNumberOfAnnotations(tasksPriority);
+                    delta += annotations - previousAnnotations;
+                }
             }
         }
-        if (delta > 0) {
-            return new RuleResult(1, String.format("%d new PMD warnings were found", delta));
-        }
         if (delta < 0) {
-            return new RuleResult(-1, String.format("%d PMD warnings were fixed", delta * -1));
+            return new RuleResult(Math.abs(delta) * pointsForRemovingAnAnnotation, 
+                    String.format("%d %s priority PMD warnings were fixed", Math.abs(delta), tasksPriority.name()));
+        }
+        if (delta > 0) {
+            return new RuleResult(Math.abs(delta) * pointsForAddingAnAnnotation, 
+                    String.format("%d new %s priority PMD warnings were found", Math.abs(delta), tasksPriority.name()));
         }
         return null;
     }
 
     public String getName() {
-        return "PMD warnings";
+        return String.format("%s priority PMD warnings", tasksPriority.name());
     }
-
 }
--- src/main/java/hudson/plugins/cigame/rules/plugins/pmd/PmdRuleSet.java
+++ src/main/java/hudson/plugins/cigame/rules/plugins/pmd/PmdRuleSet.java
@@ -1,6 +1,7 @@
 package hudson.plugins.cigame.rules.plugins.pmd;
 
 import hudson.plugins.cigame.rules.plugins.PluginRuleSet;
+import hudson.plugins.pmd.util.model.Priority;
 
 public class PmdRuleSet extends PluginRuleSet {
 
@@ -10,6 +11,8 @@ public class PmdRuleSet extends PluginRuleSet {
 
     @Override
     protected void loadRules() {
-        add(new DefaultPmdRule());
+        add(new DefaultPmdRule(Priority.HIGH, -5, 5));
+        add(new DefaultPmdRule(Priority.NORMAL, -3, 3));
+        add(new DefaultPmdRule(Priority.LOW, -1, 1));
     }
 }
