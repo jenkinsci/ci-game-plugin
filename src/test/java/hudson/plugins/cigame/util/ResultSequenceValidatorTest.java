2eb7ab45edb75261aad8263334f719cf07f6ba9a
redsolo <redsolo@dev.java.net>
2009-08-05 21:45:54 +0000

    Added util classes to check if a build sequence has a result threshold or an action.

---
+++ src/test/java/hudson/plugins/cigame/util/ResultSequenceValidatorTest.java
@@ -0,0 +1,49 @@
+package hudson.plugins.cigame.util;
+
+import static org.hamcrest.Matchers.*;
+import static org.mockito.Mockito.*;
+import static org.junit.Assert.*;
+
+import hudson.model.AbstractBuild;
+import hudson.model.Result;
+
+import org.junit.Test;
+
+@SuppressWarnings("unchecked")
+public class ResultSequenceValidatorTest {
+
+    @Test
+    public void assertResultBelowThresholdIsNotValidated() {
+        AbstractBuild build = mock(AbstractBuild.class);
+        when(build.getResult()).thenReturn(Result.FAILURE);
+        assertThat(new ResultSequenceValidator(Result.SUCCESS, 1).isValidSequence(build), is(false));
+    }
+    
+    @Test
+    public void assertShortSequenceIsNotValidated() {
+        AbstractBuild build = mock(AbstractBuild.class);
+        when(build.getResult()).thenReturn(Result.SUCCESS);
+        when(build.getPreviousBuild()).thenReturn(null);
+        assertThat(new ResultSequenceValidator(Result.SUCCESS, 2).isValidSequence(build), is(false));
+    }
+    
+    @Test
+    public void assertLastBuildIsBelowThresholdIsNotValidated() {
+        AbstractBuild build = mock(AbstractBuild.class);
+        AbstractBuild previousBuild = mock(AbstractBuild.class);
+        when(build.getResult()).thenReturn(Result.SUCCESS);
+        when(build.getPreviousBuild()).thenReturn(previousBuild);
+        when(previousBuild.getResult()).thenReturn(Result.UNSTABLE);
+        assertThat(new ResultSequenceValidator(Result.SUCCESS, 2).isValidSequence(build), is(false));
+    }
+    
+    @Test
+    public void assertBuildSequenceAboveThresholdIsValidated() {
+        AbstractBuild build = mock(AbstractBuild.class);
+        AbstractBuild previousBuild = mock(AbstractBuild.class);
+        when(build.getResult()).thenReturn(Result.SUCCESS);
+        when(build.getPreviousBuild()).thenReturn(previousBuild);
+        when(previousBuild.getResult()).thenReturn(Result.UNSTABLE);
+        assertThat(new ResultSequenceValidator(Result.UNSTABLE, 2).isValidSequence(build), is(true));
+    }
+}
