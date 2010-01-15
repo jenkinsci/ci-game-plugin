412e6857697c06e17ca5e099d0006d3a5a12973d
olamy <olamy@dev.java.net>
2010-01-15 11:15:37 +0000

    [HUDSON-4847]  [new rule] violations - stylecop.

--- src/main/java/hudson/plugins/cigame/rules/plugins/violation/ViolationsRuleSet.java
+++ src/main/java/hudson/plugins/cigame/rules/plugins/violation/ViolationsRuleSet.java
@@ -17,5 +17,6 @@ public class ViolationsRuleSet extends PluginRuleSet {
         add(new DefaultViolationRule("findbugs", Messages.ViolationRuleSet_FindBugsRule_Name(), -1, 1)); //$NON-NLS-1$ //$NON-NLS-2$
         add(new DefaultViolationRule("fxcop", Messages.ViolationRuleSet_FxcopRule_Name(), -1, 1)); //$NON-NLS-1$ //$NON-NLS-2$
         add(new DefaultViolationRule("simian", Messages.ViolationRuleSet_SimianRule_Name(), -5, 5)); //$NON-NLS-1$ //$NON-NLS-2$
+        add(new DefaultViolationRule("stylecop", Messages.ViolationRuleSet_StylecopRule_Name(), -1, 1)); //$NON-NLS-1$ //$NON-NLS-2$
     }
 }
