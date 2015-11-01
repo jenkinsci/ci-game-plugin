dc30b3c8231cbaf90f0adf3775a5d445d636a3e7
arobase-laurent <elzedo@gmail.com>
2015-11-01 20:31:59 +0100

    Avoid user to change score

--- src/main/java/hudson/plugins/cigame/UserScorePropertyDescriptor.java
+++ src/main/java/hudson/plugins/cigame/UserScorePropertyDescriptor.java
@@ -1,14 +1,14 @@
 package hudson.plugins.cigame;
 
-import net.sf.json.JSONObject;
-
 import org.kohsuke.stapler.StaplerRequest;
 
 import hudson.Extension;
 import hudson.Util;
+import hudson.model.Hudson;
 import hudson.model.User;
 import hudson.model.UserProperty;
 import hudson.model.UserPropertyDescriptor;
+import net.sf.json.JSONObject;
 
 /**
  * Descriptor for the {@link UserScoreProperty}.
@@ -37,6 +37,11 @@ public class UserScorePropertyDescriptor extends UserPropertyDescriptor {
     private UserScoreProperty newInstanceIfJSONIsNull(StaplerRequest req) throws FormException {
         String scoreStr = Util.fixEmpty(req.getParameter("game.score")); //$NON-NLS-1$
         if (scoreStr != null) {
+            if (getCurrentUserScore() != getRequestScore(scoreStr)) {
+                if (!Hudson.getInstance().getACL().hasPermission(Hudson.ADMINISTER)) {
+                    throw new hudson.model.Descriptor.FormException(Messages.UserScore_Cheating_Message(), "game.score");
+                }
+            }
             return new UserScoreProperty(Double.parseDouble(scoreStr), req.getParameter("game.participatingInGame") != null, null); //$NON-NLS-1$
         }
         return new UserScoreProperty();
@@ -44,10 +49,16 @@ public class UserScorePropertyDescriptor extends UserPropertyDescriptor {
 
     @Override
     public UserScoreProperty newInstance(StaplerRequest req, JSONObject formData) throws hudson.model.Descriptor.FormException {
+
         if (formData == null) {
             return newInstanceIfJSONIsNull(req);
         }
         if (formData.has("score")) { //$NON-NLS-1$
+            if (getCurrentUserScore() != getRequestScore(formData.get("score").toString())) {
+                if (!Hudson.getInstance().getACL().hasPermission(Hudson.ADMINISTER)) {
+                    throw new hudson.model.Descriptor.FormException(Messages.UserScore_Cheating_Message(), "score");
+                }
+            }
             return req.bindJSON(UserScoreProperty.class, formData);
         }
         return new UserScoreProperty();
@@ -56,5 +67,21 @@ public class UserScorePropertyDescriptor extends UserPropertyDescriptor {
     @Override
     public UserProperty newInstance(User arg0) {
         return null;
+    }
+
+    private double getCurrentUserScore() {
+        UserScoreProperty property = User.current().getProperty(UserScoreProperty.class);
+        return property != null ? property.getScore() : 0.0;
+    }
+
+    private double getRequestScore(String strNumber) {
+       if (strNumber != null && strNumber.length() > 0) {
+           try {
+              return Double.parseDouble(strNumber);
+           } catch(Exception e) {
+                // do nothing
+           }
+       }
+       return 0;
     }
 }
